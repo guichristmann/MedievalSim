26fd4654822340ae6e3ce8abb6916f9dcfee0160
guichristmann <guilherme.christmann@ecomp.ufsm.br>
2016-12-08 18:11:40 -0200

    Added realtime player tracking, synchronized to Firebase database.

--- app/src/main/java/org/christmann/medievalsim/GameScreen.java
+++ app/src/main/java/org/christmann/medievalsim/GameScreen.java
@@ -1,7 +1,5 @@
 package org.christmann.medievalsim;
 
-import android.*;
-import android.Manifest;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -33,6 +31,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.List;
+
 public class GameScreen extends FragmentActivity implements OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
@@ -42,14 +42,19 @@ public class GameScreen extends FragmentActivity implements OnMapReadyCallback,
     private GoogleApiClient mGoogleApiClient;
     private LocationRequest mLocationRequest;
 
-    // Firebase Authentication
+    private String characterName;   // will hold character name for database referencing
+
+    private List<Enemy> nearbyEnemies;
+
+    // Firebase stuff
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private FirebaseUser user;
+    private FirebaseDatabase database;
+    private DatabaseReference enemiesRef, usersRef, charactersRef;
 
-    // Firebase Database
-    FirebaseDatabase database;
-    DatabaseReference myRef;
+    // Markers
+    Marker pMarker; // player marker used to update player location
 
     // Tag used to log
     private static final String TAG = "GameScreen";
@@ -60,62 +65,91 @@ public class GameScreen extends FragmentActivity implements OnMapReadyCallback,
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
+
+        Log.e(TAG, "Starting GameScreen Activity");
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        firebaseAuthInit();
-
-        // Get user information
-        user = FirebaseAuth.getInstance().getCurrentUser();
-        //noinspection ConstantConditions
-        Log.d(TAG, "User: " + user.getUid());
-        if (user != null) {
-            Toast welcome = Toast.makeText(getApplicationContext(), "Welcome " + user.getEmail(), Toast.LENGTH_LONG);
-            welcome.show();
-        }
-
         // Database initialization
         database = FirebaseDatabase.getInstance();
 
+        firebaseAuthInit();
 
         mGoogleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
                 .build();
+
         // Create the LocationRequest object
         mLocationRequest = LocationRequest.create()
                 .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
-                .setFastestInterval(1000); // 1 second, in milliseconds
+                .setInterval(1000)        // in milliseconds
+                .setFastestInterval(100); // in milliseconds
 
-        //myRef.child("/Skeleton");
         setupDBListener();
     }
 
-    // Initializes Firebase Authentication
+    // Initializes Firebase Authentication and get User that is logged in
     public void firebaseAuthInit() {
         mAuth = FirebaseAuth.getInstance();
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
+                user = firebaseAuth.getCurrentUser();
                 if (user != null) {
-                    // Character is signed in
+                    // User is signed in
                     Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    Toast.makeText(getApplicationContext(), "Welcome " + user.getEmail(), Toast.LENGTH_LONG).show();
+                    getCharacterName();
                 } else {
-                    // Character is signed out
+                    // User is signed out
                     Log.d(TAG, "onAuthStateChanged:signed_out");
                 }
             }
         };
     }
 
+    // gets character name from inside User in the database
+    public void getCharacterName(){
+        // gets character from inside user in the database
+        usersRef = database.getReference("users/" + user.getUid() + "/character");
+
+        // using this because we just need to read once
+        usersRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                characterName = (String) dataSnapshot.getValue();   // gets character name from database
+                Log.e(TAG, "characterName: " + characterName);
+                if (characterName != null) {
+                    updateUserStatus(true);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "Failed to read value.", databaseError.toException());
+            }
+        });
+    }
+
+    // changes user status to online when logged in
+    // ** IMPORTANT **
+    // The first time this function is called is inside getCharacterName called from firebaseAuthInit.
+    // this is necessary to make sure we don't get a null value, which happens because firebase
+    // reads values asynchronously
+    public void updateUserStatus(boolean onlineStatus){
+        charactersRef = database.getReference("characters/" + characterName + "/online");
+        charactersRef.setValue(onlineStatus);
+    }
+
+    // Reads list of enemies from database
     public void setupDBListener(){
         // Read from the database
-        myRef = database.getReference("enemies/");
-        myRef.addValueEventListener(new ValueEventListener() {
+        enemiesRef = database.getReference("enemies/");
+        enemiesRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Log.e("Count " , ""+dataSnapshot.getChildrenCount());
@@ -138,22 +172,6 @@ public class GameScreen extends FragmentActivity implements OnMapReadyCallback,
         });
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        if (mAuth != null) {
-            mAuth.addAuthStateListener(mAuthListener);
-        }
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (mAuthListener != null) {
-            mAuth.removeAuthStateListener(mAuthListener);
-        }
-    }
-
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -166,23 +184,6 @@ public class GameScreen extends FragmentActivity implements OnMapReadyCallback,
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-    }
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (mGoogleApiClient.isConnected()) {
-            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
-            mGoogleApiClient.disconnect();
-        }
     }
 
     @Override
@@ -195,37 +196,52 @@ public class GameScreen extends FragmentActivity implements OnMapReadyCallback,
             Log.e(TAG, "No permission.");
             return;
         }
-        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
 
-        Log.d(TAG, "Location: " + location);
+        Location last_location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
 
-        if (location == null) {
-            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
-        } else {
-            handleNewLocation(location);
+        Log.d(TAG, "Location: " + last_location);
+
+        if (last_location != null) {
+            // gets last position recorded
+            LatLng lastPos =  new LatLng(last_location.getLatitude(), last_location.getLongitude());
+
+            MarkerOptions options = new MarkerOptions()
+                    .position(lastPos)
+                    .title("You");
+
+            pMarker = mMap.addMarker(options);  // creates new marker using last position recorded
+
+            handleNewPlayerLocation(last_location);
         }
-
     }
 
-    private void handleNewLocation(Location location) {
+    // Changes player Marker to new position and writes new position to database
+    private void handleNewPlayerLocation(Location location) {
         Log.d(TAG, location.toString());
 
         double currentLatitude = location.getLatitude();
         double currentLongitude = location.getLongitude();
         LatLng latLng = new LatLng(currentLatitude, currentLongitude);
 
-        MarkerOptions options = new MarkerOptions()
-                .position(latLng)
-                .title("You");
+        updateLocationDB(currentLatitude, currentLongitude);
 
-        mMap.addMarker(options);
-        mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        pMarker.setPosition(latLng); // updates marker with new position
+    }
 
+    // Writes new location of the player to the database
+    private void updateLocationDB(double lat, double lng){
+        Log.d(TAG, "Writing new location to database.");
+
+        if (characterName != null){
+            charactersRef = database.getReference("characters/" + characterName + "/lat");  // points to latitude
+            charactersRef.setValue(lat); // writes new latitude
+            charactersRef = database.getReference("characters/" + characterName + "/lng");  // points to longiutde
+            charactersRef.setValue(lng); // writes new longitude
+        }
     }
 
     private void handleNewEnemy(LatLng latLng, String name) {
-
         MarkerOptions options = new MarkerOptions()
                 .position(latLng)
                 .title(name);
@@ -257,6 +273,41 @@ public class GameScreen extends FragmentActivity implements OnMapReadyCallback,
 
     @Override
     public void onLocationChanged(Location location) {
-        handleNewLocation(location);
+        handleNewPlayerLocation(location);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+        updateUserStatus(true);    // sets character to offline
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+        updateUserStatus(false);    // sets character to offline
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (mAuth != null) {
+            mAuth.addAuthStateListener(mAuthListener);
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+        updateUserStatus(false);    // sets character to offline
+    }
+
 }
